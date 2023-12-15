07697e58111114a70c693cd334a6979b2c9c9d44
mgmg@macbook <101696461+qwercxzsda@users.noreply.github.com>
2023-12-16 04:54:07 +0900

    feat: add test code

---
+++ test.py
@@ -0,0 +1,199 @@
+import argparse
+import logging
+import os
+import time
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim
+import torch.utils.data
+import wandb
+
+from util import transform, config, dataset
+from util.util import AverageMeter, intersectionAndUnionGPU
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='PyTorch Semantic Segmentation')
+    parser.add_argument('--config', type=str, default='config/voc2012/voc2012_pspnet50_test.yaml', help='config file')
+    parser.add_argument('opts', help='see config/voc2012/voc2012_pspnet50.yaml for all options', default=None,
+                        nargs=argparse.REMAINDER)
+    args = parser.parse_args()
+    assert args.config is not None
+    cfg = config.load_cfg_from_cfg_file(args.config)
+    if args.opts is not None:
+        cfg = config.merge_cfg_from_list(cfg, args.opts)
+    return cfg
+
+
+def get_logger():
+    logger_name = "main-logger"
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    fmt = "[%(asctime)s %(levelname)s %(filename)s line %(lineno)d %(process)d] %(message)s"
+    handler.setFormatter(logging.Formatter(fmt))
+    logger.addHandler(handler)
+    return logger
+
+
+def check(args):
+    assert args.classes > 1
+    assert args.zoom_factor in [1, 2, 4, 8]
+    assert args.arch == 'psp' or args.arch == 'psp_modified'
+    assert (args.train_h - 1) % 8 == 0 and (args.train_w - 1) % 8 == 0
+
+
+class Writer:
+    def __init__(self):
+        from torch.utils.tensorboard import SummaryWriter
+        self.writer = SummaryWriter()
+
+    def add_scalar(self, tag, scalar_value, global_step):
+        self.writer.add_scalar(tag, scalar_value, global_step)
+        wandb.log({tag: scalar_value})
+
+
+args = get_parser()
+check(args)
+logger = get_logger()
+writer = Writer()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def main():
+    assert torch.cuda.is_available()
+    os.environ["CUDA_VISIBLE_DEVICES"] = ','.join(str(x) for x in args.train_gpu)
+    assert len(args.train_gpu) == 1
+    args.sync_bn = False
+    args.distributed = False
+    args.multiprocessing_distributed = False
+    wandb.config.update(args)
+
+    criterion = nn.CrossEntropyLoss(ignore_index=args.ignore_label)
+    assert args.arch == 'psp' or args.arch == 'psp_modified'
+    if args.arch == 'psp':
+        from model.pspnet import PSPNet
+        model = PSPNet(layers=args.layers, classes=args.classes, zoom_factor=args.zoom_factor, criterion=criterion)
+    else:
+        from model.pspnet_modified import PSPNet
+        model = PSPNet(layers=args.layers, classes=args.classes, zoom_factor=args.zoom_factor, criterion=criterion)
+    modules_ori = [model.layer0, model.layer1, model.layer2, model.layer3, model.layer4]
+    modules_new = [model.ppm, model.cls, model.aux]
+
+    params_list = []
+    for module in modules_ori:
+        params_list.append(dict(params=module.parameters(), lr=args.base_lr))
+    for module in modules_new:
+        params_list.append(dict(params=module.parameters(), lr=args.base_lr * 10))
+    args.index_split = 5
+    optimizer = torch.optim.SGD(params_list, lr=args.base_lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    if args.sync_bn:
+        model = nn.SyncBatchNorm.convert_sync_batchnorm(model)
+
+    logger.info(args)
+    logger.info("=> creating model ...")
+    logger.info("Classes: {}".format(args.classes))
+    logger.info(model)
+
+    model = model.to(device)
+
+    assert args.weight is not None
+    if os.path.isfile(args.weight):
+        logger.info("=> loading weight '{}'".format(args.weight))
+        checkpoint = torch.load(args.weight)
+        model.load_state_dict(checkpoint['state_dict'])
+        logger.info("=> loaded weight '{}'".format(args.weight))
+    else:
+        logger.info("=> no weight found at '{}'".format(args.weight))
+
+
+    value_scale = 255
+    mean = [0.485, 0.456, 0.406]
+    mean = [item * value_scale for item in mean]
+    std = [0.229, 0.224, 0.225]
+    std = [item * value_scale for item in std]
+
+    assert not args.distributed
+    if args.evaluate:
+        val_transform = transform.Compose([
+            transform.Crop([args.train_h, args.train_w], crop_type='center', padding=mean,
+                           ignore_label=args.ignore_label),
+            transform.ToTensor(),
+            transform.Normalize(mean=mean, std=std)])
+        val_data = dataset.SemData(split='val', data_root=args.data_root, data_list=args.val_list,
+                                   transform=val_transform)
+        assert not args.distributed
+        assert args.workers == 0
+        val_loader = torch.utils.data.DataLoader(val_data, batch_size=args.batch_size_val, shuffle=False)
+
+        loss_val, mIoU_val, mAcc_val, allAcc_val = validate(val_loader, model, criterion)
+        writer.add_scalar('loss_val', loss_val, 0)
+        writer.add_scalar('mIoU_val', mIoU_val, 0)
+        writer.add_scalar('mAcc_val', mAcc_val, 0)
+        writer.add_scalar('allAcc_val', allAcc_val, 0)
+
+
+def validate(val_loader, model, criterion):
+    logger.info('>>>>>>>>>>>>>>>> Start Evaluation >>>>>>>>>>>>>>>>')
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    loss_meter = AverageMeter()
+    intersection_meter = AverageMeter()
+    union_meter = AverageMeter()
+    target_meter = AverageMeter()
+
+    model.eval()
+    end = time.time()
+    for i, (input, target) in enumerate(val_loader):
+        data_time.update(time.time() - end)
+        input = input.to(device)
+        target = target.to(device)
+        output = model(input)
+        if args.zoom_factor != 8:
+            output = F.interpolate(output, size=target.size()[1:], mode='bilinear', align_corners=True)
+        loss = criterion(output, target)
+
+        n = input.size(0)
+        assert not args.multiprocessing_distributed
+        loss = torch.mean(loss)
+
+        output = output.max(1)[1]
+        intersection, union, target = intersectionAndUnionGPU(output, target, args.classes, args.ignore_label)
+        intersection, union, target = intersection.cpu().numpy(), union.cpu().numpy(), target.cpu().numpy()
+        intersection_meter.update(intersection), union_meter.update(union), target_meter.update(target)
+
+        accuracy = sum(intersection_meter.val) / (sum(target_meter.val) + 1e-10)
+        loss_meter.update(loss.item(), input.size(0))
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+    if ((i + 1) % args.print_freq == 0):
+        logger.info('Test: [{}/{}] '
+                    'Data {data_time.val:.3f} ({data_time.avg:.3f}) '
+                    'Batch {batch_time.val:.3f} ({batch_time.avg:.3f}) '
+                    'Loss {loss_meter.val:.4f} ({loss_meter.avg:.4f}) '
+                    'Accuracy {accuracy:.4f}.'.format(i + 1, len(val_loader),
+                                                      data_time=data_time,
+                                                      batch_time=batch_time,
+                                                      loss_meter=loss_meter,
+                                                      accuracy=accuracy))
+
+    iou_class = intersection_meter.sum / (union_meter.sum + 1e-10)
+    accuracy_class = intersection_meter.sum / (target_meter.sum + 1e-10)
+    mIoU = np.mean(iou_class)
+    mAcc = np.mean(accuracy_class)
+    allAcc = sum(intersection_meter.sum) / (sum(target_meter.sum) + 1e-10)
+
+    logger.info('Val result: mIoU/mAcc/allAcc {:.4f}/{:.4f}/{:.4f}.'.format(mIoU, mAcc, allAcc))
+    for i in range(args.classes):
+        logger.info('Class_{} Result: iou/accuracy {:.4f}/{:.4f}.'.format(i, iou_class[i], accuracy_class[i]))
+    logger.info('<<<<<<<<<<<<<<<<< End Evaluation <<<<<<<<<<<<<<<<<')
+    return loss_meter.avg, mIoU, mAcc, allAcc
+
+
+if __name__ == '__main__':
+    wandb.init(project="computer-vision")
+    main()
